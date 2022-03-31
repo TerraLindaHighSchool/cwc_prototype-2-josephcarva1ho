77f8c41b5739603513191132cd55fba4a19c3757
josephcarva1ho <89489503+josephcarva1ho@users.noreply.github.com>
2022-03-31 14:46:24 -0700

    Activity 1 completed

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 10.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        //Keep the Player in bounds
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+    }
+
+}
